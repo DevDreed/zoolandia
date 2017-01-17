17ce76f8976983a7fdce86904c10213c4250c591
Dustin Reed <dmreedy1@gmail.com>
2017-01-16 20:28:38 -0600

    finished lesson 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Zoolandia.Animals;
+
+namespace Zoolandia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lion = new Lion();
+            lion.name = "Dustin";
+            lion.alpha = true;
+            System.Console.WriteLine($"The Lion's name is {lion.name} and alpha = {lion.alpha}"); 
+
+            var bear = new Bear();
+            bear.name = "New bear";
+            bear.height = 10;
+            bear.weight = 600;
+            bear.clawLength = 2;
+            bear.skullMeasuremnt = 12;
+
+            System.Console.WriteLine($"{bear.height}.{bear.weight}.{bear.name}"); 
+
+            var zebra = new Lion();
+            zebra.name = "new zebra";
+            System.Console.WriteLine($"{zebra.height}.{zebra.weight}.{zebra.name}"); 
+        }
+    }
+}
